7f5757975e8887c397bc7565c394a60042ecabae
Paola <paoalm1@hotmail.com>
2024-05-18 20:55:11 -0500

    feat: se da estilo a la pantalla del home

--- Frontend/app/src/main/java/com/udistrital/soviet_paws/views/HomeScreen.kt
+++ Frontend/app/src/main/java/com/udistrital/soviet_paws/views/HomeScreen.kt
@@ -1,6 +1,11 @@
 package com.udistrital.soviet_paws.views
 
+import android.content.res.Resources
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarToday
@@ -14,21 +19,25 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.udistrital.soviet_paws.R
 import kotlinx.coroutines.launch
 
 
@@ -90,9 +99,34 @@ fun HomeScreen(navController: NavController){
 
 @Composable
 fun Home(navController: NavController) {
-    Column {
-        Text(text = "Home")
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+        color = colorResource(R.color.soviet_red)) {
+        Column(
+            modifier = Modifier.padding(10.dp, 30.dp)
+        ) {
+            Image(
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(5.dp)
+                    .height(400.dp)
+                    .align(Alignment.CenterHorizontally),
+                painter = painterResource(R.drawable.pet_icon),
+                contentDescription = "pet-photo")
+            Text(
+                text = "Bienvenido a",
+                fontSize = 20.sp,
+                color = Color.White)
+
+            Text(
+                text = "Soviet Pets",
+                fontWeight = FontWeight.Bold,
+                fontSize = 40.sp,
+                color = Color.White)
+        }
     }
+
 
 }
 @Composable
@@ -105,7 +139,7 @@ fun MyNavigationDrawerItem(
 ) {
     NavigationDrawerItem(
         colors = NavigationDrawerItemDefaults.colors(
-            selectedContainerColor = Color.Cyan,
+            selectedContainerColor = colorResource(R.color.soviet_red),
         ),
         modifier = Modifier.padding(6.dp),
         label = { Text(text = label, fontWeight = FontWeight(500)) },
--- Frontend/app/src/main/java/com/udistrital/soviet_paws/views/ListPets.kt
+++ Frontend/app/src/main/java/com/udistrital/soviet_paws/views/ListPets.kt
@@ -1,14 +1,97 @@
 package com.udistrital.soviet_paws.views
 
+import android.content.res.Resources
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyHorizontalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.udistrital.soviet_paws.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListPets(navController: NavController){
-    Column {
-        Text(text = "List")
-    }
+    Surface(
+        color = colorResource(R.color.soviet_red)) {
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        contentPadding = PaddingValues(5.dp),
+        horizontalArrangement = Arrangement.spacedBy(5.dp),
+    ){
+        val itemsList = (0..10).toList()
+        items(itemsList){ item ->
+            ElevatedCard(
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White,
+                    contentColor = Color.Black
+                ),
+                modifier = Modifier
+                    .padding(10.dp)
+                    .height(300.dp),
+                shape = RoundedCornerShape(10.dp),
+                onClick = { /*TODO*/ }) {
+                Image(
+                    alignment = Alignment.Center,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .height(100.dp)
+                        .align(Alignment.CenterHorizontally),
+                    painter = painterResource(R.drawable.pet_icon),
+                    contentDescription = "pet-photo")
+                Text(
+                    stringResource(R.string.name, item),
+                    fontSize = 30.sp,
+                    modifier = Modifier.
+                padding(10.dp))
+                Text(
+                    stringResource(R.string.tipo, item),
+                    modifier = Modifier.
+                    padding(10.dp))
 
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth()
+                        .align(Alignment.CenterHorizontally),
+                    colors = ButtonDefaults
+                        .buttonColors(
+                            containerColor = colorResource(R.color.soviet_yellow)),
+                    onClick = { /*TODO*/ }) {
+                    Text(stringResource(R.string.details), color = Color.Black)
+                }
+            }
+        }
+    }
+    }
 }
+
